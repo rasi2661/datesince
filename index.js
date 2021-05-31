56d5a72a3481dcb2f1176d64a2b40ea03c26bced
rasi2661 <70203942+rasi2661@users.noreply.github.com>
2021-05-31 10:41:54 +0200

    added date functions

---
+++ index.js
@@ -0,0 +1,40 @@
+function yearsSince(longago, today) {
+  let diffTime = Math.abs(today - longago);
+  let diffYears = diffTime / (1000 * 60 * 60 * 24 * 365);
+  return diffYears;
+}
+
+function daysSince(longago, today) {
+  let diffTime = Math.abs(today - longago);
+  let diffDays = diffTime / (1000 * 60 * 60 * 24);
+  return diffDays;
+}
+
+function hoursSince(longago, today) {
+  let diffTime = Math.abs(today - longago);
+  let diffHours = diffTime / (1000 * 60 * 60);
+  return diffHours;
+}
+
+function secondsSince(longago, today) {
+  let diffTime = Math.abs(today - longago);
+  let diffSeconds = diffTime / (1000 * 60);
+  return diffSeconds;
+}
+
+function msSince(longago, today) {
+  let diffTime = Math.abs(today - longago);
+  let diffMS = diffTime / (1000);
+  return diffMS;
+}
+
+function dateSince(longago) {
+  let today = new Date();
+  return {
+    years: yearsSince(longago, today),
+    days: daysSince(longago, today),
+    hours: hoursSince(longago, today),
+    seconds: secondsSince(longago, today),
+    milliseconds: msSince(longago, today)
+  }
+}
